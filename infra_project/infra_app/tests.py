6c92e9e653bfeaafaf5a106a62e502a632e1bad0
pbutsik <p.butsik@gmail.com>
2022-12-11 11:06:43 +0300

    add test with test

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -21,5 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
-        print(response)
         self.assertContains(response, 'А это вторая страница!')
